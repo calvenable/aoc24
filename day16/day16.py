a0c0962a238fae7735784e8c2a045dfca0f875d1
Caleb Venable <calvenable@gmail.com>
2024-12-24 21:40:26 +0000

    Completed Day 16 Part 2 (finally)

--- day16/day16.py
+++ day16/day16.py
@@ -35,7 +35,7 @@ def partOne(inputFilePath):
 
   lowest = 100000000000
   lowestToLoc:list[Reindeer] = []
-  count = 0
+  
   while (reindeers):
     reindeer = reindeers.pop(0)
 
@@ -93,23 +93,103 @@ def partOne(inputFilePath):
           if map[reindeer.x-1][reindeer.y] != '#':
             reindeers.append(Reindeer(reindeer.x, reindeer.y, (reindeer.dir + 3) % len(Direction), reindeer.prev, True, reindeer.score + 1000))
 
-    count += 1
-    if count % 1000000 == 0:
-      print("lowest: " + str(lowest) + " at iteration " + str(count/1000000) + " million, reindeer left = " + str(len(reindeers)))
   return lowest
 
 
 # Code for Part Two ----------------------------------------------------
-def partTwo(inputFilePath):
-  return False
+def partTwo(inputFilePath, lowest):
+
+  map = []
+  reindeers:list[Reindeer] = []
+
+  x=0
+  with open(inputFilePath, "r") as file:
+    for line in file:
+      map.append(list(line.strip()))
+      if 'S' in line:
+        reindeers.append(Reindeer(x, line.index('S'), Direction.RIGHT))
+      x += 1
+
+  successfulReindeer:list[Reindeer] = []
+  lowestToLoc:list[Reindeer] = []
+
+  
+  while (reindeers):
+    reindeer = reindeers.pop(0)
+
+    if reindeer.score > lowest:
+      continue
+
+    # Keep a list of the lowest score to get to every location
+    try:
+      ind = lowestToLoc.index(reindeer)
+      lowestSoFar = lowestToLoc[ind]
+      if reindeer.score < lowestSoFar.score:
+        lowestToLoc[ind] = reindeer
+      elif reindeer.score == lowestSoFar.score:
+        lowestToLoc[ind].prev = list(set(reindeer.prev).union(set(lowestSoFar.prev)))
+      else:
+        continue
+    except ValueError:
+      lowestToLoc.append(reindeer)
+
+    if map[reindeer.x][reindeer.y] == 'E':
+      successfulReindeer.append(lowestToLoc[lowestToLoc.index(reindeer)])
+      continue
+
+    # Try: move forward, turn left (if not turned before), turn right (if not turned on this space before)
+    match reindeer.dir:
+      case Direction.UP:
+        if map[reindeer.x-1][reindeer.y] != '#' and (reindeer.x - 1, reindeer.y) not in reindeer.prev:
+          reindeers.append(Reindeer(reindeer.x - 1, reindeer.y, reindeer.dir, reindeer.prev + [(reindeer.x, reindeer.y)], False, reindeer.score + 1))
+        if not reindeer.hasTurned:
+          if map[reindeer.x][reindeer.y+1] != '#':
+            reindeers.append(Reindeer(reindeer.x, reindeer.y, (reindeer.dir + 1) % len(Direction), reindeer.prev, True, reindeer.score + 1000))
+          if map[reindeer.x][reindeer.y-1] != '#':
+            reindeers.append(Reindeer(reindeer.x, reindeer.y, (reindeer.dir + 3) % len(Direction), reindeer.prev, True, reindeer.score + 1000))
+      
+      case Direction.DOWN:
+        if map[reindeer.x+1][reindeer.y] != '#' and (reindeer.x + 1, reindeer.y) not in reindeer.prev:
+          reindeers.append(Reindeer(reindeer.x + 1, reindeer.y, reindeer.dir, reindeer.prev + [(reindeer.x, reindeer.y)], False, reindeer.score + 1))
+        if not reindeer.hasTurned:
+          if map[reindeer.x][reindeer.y+1] != '#':
+            reindeers.append(Reindeer(reindeer.x, reindeer.y, (reindeer.dir + 3) % len(Direction), reindeer.prev, True, reindeer.score + 1000))
+          if map[reindeer.x][reindeer.y-1] != '#':
+            reindeers.append(Reindeer(reindeer.x, reindeer.y, (reindeer.dir + 1) % len(Direction), reindeer.prev, True, reindeer.score + 1000))
+      
+      case Direction.LEFT:
+        if map[reindeer.x][reindeer.y-1] != '#' and (reindeer.x, reindeer.y - 1) not in reindeer.prev:
+          reindeers.append(Reindeer(reindeer.x, reindeer.y - 1, reindeer.dir, reindeer.prev + [(reindeer.x, reindeer.y)], False, reindeer.score + 1))
+        if not reindeer.hasTurned:
+          if map[reindeer.x-1][reindeer.y] != '#':
+            reindeers.append(Reindeer(reindeer.x, reindeer.y, (reindeer.dir + 1) % len(Direction), reindeer.prev, True, reindeer.score + 1000))
+          if map[reindeer.x+1][reindeer.y] != '#':
+            reindeers.append(Reindeer(reindeer.x, reindeer.y, (reindeer.dir + 3) % len(Direction), reindeer.prev, True, reindeer.score + 1000))
+      
+      case Direction.RIGHT:
+        if map[reindeer.x][reindeer.y+1] != '#' and (reindeer.x, reindeer.y + 1) not in reindeer.prev:
+          reindeers.append(Reindeer(reindeer.x, reindeer.y + 1, reindeer.dir, reindeer.prev + [(reindeer.x, reindeer.y)], False, reindeer.score + 1))
+        if not reindeer.hasTurned:
+          if map[reindeer.x+1][reindeer.y] != '#':
+            reindeers.append(Reindeer(reindeer.x, reindeer.y, (reindeer.dir + 1) % len(Direction), reindeer.prev, True, reindeer.score + 1000))
+          if map[reindeer.x-1][reindeer.y] != '#':
+            reindeers.append(Reindeer(reindeer.x, reindeer.y, (reindeer.dir + 3) % len(Direction), reindeer.prev, True, reindeer.score + 1000))
+
+  
+  tilesOnShortestPaths = set()
+  for reindeer in successfulReindeer:
+    for tile in reindeer.prev:
+      tilesOnShortestPaths.add(tile)
+
+  return len(tilesOnShortestPaths) + 1
 
 
 # Run the code for the specified part ----------------------------------
 # answer = partOne("day16/test.txt")
-answer = partOne("day16/input.txt")
+# answer = partOne("day16/input.txt")
 
-# answer = partTwo("day16/test.txt")
-# answer = partTwo("day16/input.txt")
+# answer = partTwo("day16/test.txt", 11048)
+answer = partTwo("day16/input.txt", 133584)
 
 pyperclip.copy(answer)
 print(answer)
